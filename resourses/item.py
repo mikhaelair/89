263f184e468da4f8f3810094fa2946fc69fe92b1
mikhaelairapetov <mikhael.air@gmail.com>
2022-07-12 10:49:27 +0400

    upload

---
+++ resourses/item.py
@@ -0,0 +1,74 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+import sqlite3
+from models.item import ItemModel
+from db import db
+
+
+class Item(Resource):
+    TABLE_NAME = 'items'
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!"
+                        )
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'message': 'Item not found'}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}
+
+        data = Item.parser.parse_args()
+
+        item = ItemModel(name, data['price'])
+
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item."}
+
+        return item.json()
+
+    @jwt_required()
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+
+        return {'message': 'Item deleted'}
+
+    @jwt_required()
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(name)
+
+        if item is None:
+            item = ItemModel(name, data['price'])
+        else:
+            item.price = data['price']
+
+        item.save_to_db()
+        return item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+        connection.close()
+
+        return {'items': items}
